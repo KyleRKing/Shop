903a41b769d93d5b0f0ff8e9f09046d93411b309
Tali Walt <taliwalt@gmail.com>
2015-07-31 04:21:45 -0700

    Seeded the database with a csv of products exported from a WordPress site I created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+CSV.foreach(Rails.root.join("db/seeds_data/product1.csv"), headers: true) do |row|
+  Product.find_or_create_by(name: row[0], price: row[1], image: row[2], description: row[3])
+end
